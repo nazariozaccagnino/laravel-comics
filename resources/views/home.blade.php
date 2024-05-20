4c9d86b52174157b7e3c038d7916b11bf197bea7
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-20 17:11:43 +0200

    cards placed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,28 @@
 @section('title', 'Home')
 
 @section('content')
-<section class="container">
-    <ul>
-    @foreach ( $comics as $items)
-        <li>{{$items['title']}}</li>
+<div class="jumbo">
+    <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="jumbo">
+</div>
+<section>
+    <div class="container d-flex flex-wrap">
+        @foreach ($comics as $items)
+            <div class="row">
+                <div class="col d-flex align-items-stretch p-2 m-2">
+                    <div class="card">
+                        <img src="{{$items['thumb']}}" class="card-img-top thumb" style="width: 18rem;" alt="descr">
+                        <div class="card-body">
+                            <!-- <h5 class="card-title">{{$items['title']}}</h5> -->
+                            <!-- <p class="card-text">{{$items['description']}}</p> -->
+                        </div>
+                    </div>
+                </div>
 
-    @endforeach
-    </ul>
+            </div>
+
+        @endforeach
+    </div>
+
 </section>
 
 @endsection
